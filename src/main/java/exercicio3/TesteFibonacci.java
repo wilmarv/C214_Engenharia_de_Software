48468080d743bffcd9ebcd9ae2f8a86114cdb200
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2023-03-10 22:02:41 -0300

    atualiza codigo de teste para Junit5

--- src/main/java/exercicio3/TesteFibonacci.java
+++ src/main/java/exercicio3/TesteFibonacci.java
@@ -1,8 +1,10 @@
 package exercicio3;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
 
 public class TesteFibonacci {
 
--- src/main/java/exercicio4/TesteEmptyStackException.java
+++ src/main/java/exercicio4/TesteEmptyStackException.java
@@ -3,16 +3,19 @@ package exercicio4;
 import java.util.EmptyStackException;
 import java.util.Stack;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TesteEmptyStackException {
 
-	@Test(expected = EmptyStackException.class)
-	public void testEmptyStackException() {
-		Stack<Integer> stack = new Stack();
+	@Test
+	void testEmptyStackException() {
+		final Stack<Integer> stack = new Stack();
 		stack.push(10);
-		stack.pop();
-		stack.pop();
+		Assertions.assertThrows(EmptyStackException.class, () -> {
+			stack.pop();
+			stack.pop();
+		});
 	}
 
 }
