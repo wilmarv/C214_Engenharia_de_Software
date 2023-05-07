ad965f45261dcc6462492029ba090d5a4efe2793
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2023-05-07 12:25:09 -0300

    Inicia projeto para testes com mock

---
+++ android/app/src/main/kotlin/com/example/test_mock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_mock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/compels/FlutterSDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/compels/Documents/git/C214_Engenharia_de_Software"
+export "FLUTTER_APPLICATION_PATH=/Users/compels/Documents/git/test_mock"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
